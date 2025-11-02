4323538cd763166f5690dddd2c5dceac425a9bb0
Anushka Patare <anupatare07@gmail.com>
2025-11-02 14:20:46 +0530

    Add files via upload

---
+++ Assignments/Assignment13/Assignment13_5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int sum_even_numbers(int limit)
+{
+    int iCnt = 0,iSum=0;
+    for(iCnt=1; iCnt<=limit; iCnt++)
+    {
+       if(iCnt%2==0)
+        {
+
+           iSum = iSum+iCnt;
+
+        }
+    }
+    return iSum;
+}
+
+int main()
+{
+    int limit=0,iRet=0;
+
+    printf("Enter number:");
+    scanf("%d",&limit);
+
+    printf("%d\n",sum_even_numbers(limit));
+
+    return 0;
+}
