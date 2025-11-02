085faa791ae68f8afd32fd657879b0590f071cf4
Anushka Patare <anupatare07@gmail.com>
2025-11-02 14:10:16 +0530

    Add files via upload

---
+++ Assignment10/Assignment10_5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+double SquareMeter(int iValue)
+{
+    double dSqMeter = 0.0;
+
+    dSqMeter = iValue * 0.0929;
+
+    return dSqMeter;
+
+}
+int main()
+{
+    int iValue = 0;
+    double dRet = 0.0;
+
+    printf("Enter area in square feet :");
+    scanf("%d",&iValue);
+
+    dRet = SquareMeter(iValue);
+    printf("Area in square meter is :%f",dRet);
+
+    return 0;
+}
