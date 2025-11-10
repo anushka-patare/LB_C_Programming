401637ac52d0874f2f3a47e6a3127dc6c501e0c8
Anushka Patare <anupatare07@gmail.com>
2025-11-10 23:21:58 +0530

    Add files via upload

---
+++ Assignments/Assignment11/Assignment11_3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int RangeSum(int iStart,int iEnd)
+{
+    int iCnt=0;
+    int iSum=0;
+
+    if(iStart>iEnd)
+    {
+        printf("Invalid range");
+        return 0;
+    }
+
+    if(iStart <0 || iEnd < 0)
+    {
+        printf("Invalid range");
+        return 0;
+    }
+
+    for(iCnt=iStart; iCnt<=iEnd; iCnt++)
+    {
+        iSum = iSum + iCnt;
+    }
+    return iSum;
+}
+int main()
+{
+    int iValue1 =0,iValue2 = 0,iRet=0;
+    
+    printf("Enter starting point:");
+    scanf("%d",&iValue1);
+
+    printf("Enter ending point");
+    scanf("%d",&iValue2);
+
+    iRet = RangeSum(iValue1,iValue2);
+
+    if(iRet != 0)
+    {
+        printf("Addition is %d",iRet);
+    }
+    return 0;
+}
