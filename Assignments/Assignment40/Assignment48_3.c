ea91df8d796332e2e2964b858773bcf29abc3286
Anushka Patare <anupatare07@gmail.com>
2026-01-05 23:31:29 +0530

    Add files via upload

---
+++ Assignments/Assignment40/Assignment48_3.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head,int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->next = NULL;
+    newn->data = no;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->next = *Head;
+        *Head = newn;
+    }
+}
+
+void Display(PNODE Head)
+{
+    
+    while(Head != NULL)
+    {
+        printf("| %d | -> ",Head->data);
+        Head = Head->next;
+    }
+    printf("NULL\n");
+}
+
+void MultiplyByTwo(PPNODE Head)
+{
+    PNODE temp = NULL;
+
+    temp = *Head;
+    while(temp != NULL)
+    {
+        temp->data = temp->data * 2;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    PNODE first = NULL;
+    int iCnt = 0,len = 0, no = 0;
+    bool bRet = false;
+    printf("Enter how many nodes you want into linked list :\n");
+    scanf("%d",&len);
+
+    for(iCnt=1; iCnt<=len; iCnt++)
+    {
+        printf("Enter element into node %d :",iCnt);
+        scanf("%d",&no);
+        InsertFirst(&first,no);
+    }
+
+    Display(first);
+
+    MultiplyByTwo(&first);
+    
+    Display(first);
+    
+    return 0;
+}
