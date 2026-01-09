66391c8632e66fddebc0e90bb7cdec37d5b6341a
Anushka Patare <anupatare07@gmail.com>
2026-01-09 14:46:53 +0530

    Add files via upload

---
+++ Assignments/Assignment42/Assignment50_2.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+void InsertFirst(PPNODE Head,int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->next = NULL;
+    newn->data = no;
+
+    if(*Head == NULL)
+    {
+        *Head = newn;
+    }
+    else
+    {
+        newn->next = *Head;
+        *Head = newn;
+    }
+}
+
+void Display(PNODE Head)
+{
+    
+    while(Head != NULL)
+    {
+        printf("| %d | -> ",Head->data);
+        Head = Head->next;
+    }
+    printf("NULL\n");
+}
+
+void DisplayGreaterThanAvg(PNODE Head)
+{
+    PNODE temp = NULL;
+    temp = Head;
+
+    int iSum = 0;
+    int iFreq = 0;
+    
+    while(temp != NULL)
+    {
+        iSum =  iSum + temp->data;
+        temp = temp->next;
+        iFreq++;   
+    }
+
+    float iAvg = 0.0f;
+    iAvg = iSum / iFreq;
+
+    temp = Head;
+    while(temp != NULL)
+    {
+        if(temp->data > iAvg)
+        {
+            printf("%d\n",temp->data);
+        }
+        temp  = temp->next;
+    }
+}
+int main()
+{
+    PNODE first = NULL;
+    int iCnt = 0,len = 0, no = 0;
+    bool bRet = false;
+    printf("Enter how many nodes you want into linked list :\n");
+    scanf("%d",&len);
+
+    for(iCnt=1; iCnt<=len; iCnt++)
+    {
+        printf("Enter element into node %d :",iCnt);
+        scanf("%d",&no);
+        InsertFirst(&first,no);
+    }
+
+    Display(first);
+
+    DisplayGreaterThanAvg(first);
+    
+    return 0;
+}
